af12ae71e4b03018db822d55c9edbe02e1ff12fc
raheelmughal <raheelmughal018@gmail.com>
2025-07-26 01:22:34 +0500

    add column of unit_price in stock

--- app/models/stock.py
+++ app/models/stock.py
@@ -11,5 +11,6 @@ class Stock(db.Model):
     stock_id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     item_id = db.Column(db.String(36), db.ForeignKey('items.item_id'))
     quantity = db.Column(db.Integer, nullable=False)
+    unit_price = db.Column(db.Integer,nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
